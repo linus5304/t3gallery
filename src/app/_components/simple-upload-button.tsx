a7b6d0e9ea97edd62ad1465888551cde0d532e92
linusbayere <linusbayere@gmail.com>
2024-04-16 23:41:34 +0100

    Add schad sonner toast

--- src/app/_components/simple-upload-button.tsx
+++ src/app/_components/simple-upload-button.tsx
@@ -1,6 +1,6 @@
 import { useRouter } from "next/navigation";
 import { useUploadThing } from "~/utils/uploadthing";
-
+import { toast } from "sonner";
 // inferred input off useUploadThing
 type Input = Parameters<typeof useUploadThing>;
 
@@ -49,7 +49,15 @@ function UploadSVG() {
 export function SimpleUploadButton() {
   const router = useRouter();
   const { inputProps } = useUploadThingInputProps("imageUploader", {
+    onUploadBegin() {
+      toast("Uploading...", {
+        duration: 100000,
+        id: "upload-begin"
+      });
+    },
     onClientUploadComplete(res) {
+      toast.dismiss("upload-begin");
+      toast("Upload complete!");
       router.refresh();
     },
   });
