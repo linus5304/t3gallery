54ffa6646996a9ec914a8cc82980a157309402d3
linusbayere <linusbayere@gmail.com>
2024-04-16 19:04:31 +0100

    scafold IU

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,23 +4,23 @@ const mockUrls = [
   "https://utfs.io/f/a969b305-2709-49c9-8f5b-b1bcd01d2238-uspbdk.png",
   "https://utfs.io/f/be871ffb-be68-4633-bb45-4b121e626e27-1d.png",
   "https://utfs.io/f/4765bab7-eee0-404d-bb4b-f427c56b06e8-1e.png",
-  "https://utfs.io/f/efc6d006-270e-4e8c-8087-07b5911eadb8-1f.png"
+  "https://utfs.io/f/efc6d006-270e-4e8c-8087-07b5911eadb8-1f.png",
 ];
 
 const mockImages = mockUrls.map((url, index) => ({
   id: index + 1,
-  url
+  url,
 }));
 
 export default function HomePage() {
   return (
     <main className="">
-      <div className="flex flex-wrap">
-        {mockImages.map(image => (
-          <div key={image.id} className="w1/2 p-4">
-             <img src={image.url} />
-            </div>
-        )) }
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} />
+          </div>
+        ))}
       </div>
     </main>
   );
